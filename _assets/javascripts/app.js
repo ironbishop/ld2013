f06ea53321026cc78ac23e8d52166b425867f6c1
Federico Fissore <federico@fissore.org>
2013-10-15 20:43:57 +0200

    introducing jekyll-assets

---
+++ _assets/javascripts/app.js
@@ -0,0 +1,7 @@
+//= require jquery.min.js
+//= require scripts.js
+//= require jquery.countdown.min.js
+//= require jquery.countdown-it.js
+//= require jquery.easing.1.3.js
+//= require jquery.flexslider-min.js
+//= require prettyPhoto/js/jquery.prettyPhoto.js
